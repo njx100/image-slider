e8b85eb136bce0c5a211a81ffc744b2bce49a936
njx100 <dnnguyen100@gmail.com>
2023-04-06 20:24:13 +0700

    added auto slider and slide animation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const btnNextEl = document.querySelector(".btn-next");
 const imageContainerEl = document.querySelector(".image-container");
 const numb = imageContainerEl.childElementCount;
 let currentImg = 1;
+let timer;
 
 function checkCurrentImg() {
     if (currentImg < 1) {
@@ -16,19 +17,28 @@ function nextImage() {
     currentImg++;
     checkCurrentImg();
     updateImage();
+    clearTimeout(timer);
+    autoSlider();
 }
 
 function previosImage() {
     currentImg--;
     checkCurrentImg();
     updateImage();
+    clearTimeout(timer);
+    autoSlider();
 }
 
 function updateImage() {
     imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * (500)}px)`;
 };
 
-
-
 btnNextEl.addEventListener("click", nextImage);
 btnPreviousEl.addEventListener("click", previosImage);
+
+function autoSlider() {timer = setTimeout(() => {
+   nextImage();
+}, 3000);
+};
+
+autoSlider();
