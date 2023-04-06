d129c688d0c364735204ae4b3c16e8461bbfbd36
njx100 <dnnguyen100@gmail.com>
2023-04-06 19:53:38 +0700

    all basic functions

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+const btnPreviousEl = document.querySelector(".btn-previous")
+const btnNextEl = document.querySelector(".btn-next");
+const imageContainerEl = document.querySelector(".image-container");
+const numb = imageContainerEl.childElementCount;
+let currentImg = 1;
 
+function checkCurrentImg() {
+    if (currentImg < 1) {
+        currentImg = numb;
+    } else if (currentImg > numb) {
+        currentImg = 1;
+    }
+};
+
+function nextImage() {
+    currentImg++;
+    checkCurrentImg();
+    updateImage();
+}
+
+function previosImage() {
+    currentImg--;
+    checkCurrentImg();
+    updateImage();
+}
+
+function updateImage() {
+    imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * (500)}px)`;
+};
+
+
+
+btnNextEl.addEventListener("click", nextImage);
+btnPreviousEl.addEventListener("click", previosImage);
